6cdf13ac74f309fabc8f1f4f228dd5b2d11e8389
MATHIAS MORAIS LIMA <mathiaslima@A305-28L.imd.local>
2020-02-20 12:26:59 -0300

    Update

---
+++ include/my_lib.h
@@ -0,0 +1,8 @@
+#define MY_LIB_H
+#ifdef MY_LIB_H
+
+#include <string>
+using std::string;
+void func( std::string );
+
+#endif
---
+++ src/my_lib.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#include "../include/my_lib.h"
+
+
+void func(string msg ){
+   std::cout << ">>> Recebi a mensagem \"" << msg << "\"\n";
+}
+
