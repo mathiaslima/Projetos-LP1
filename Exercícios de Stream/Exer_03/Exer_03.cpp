daa3d9dfdc3060375309ad5b63833a181003e173
unknown <mathias.morais7@gmail.com>
2020-03-14 23:16:28 -0300

    iniciando e completando o exercicio 3

---
+++ Exercícios de Stream/Exer_03/Exer_03.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+
+int getInteger( std::string inputStr_ ){
+    int value;
+    std::string number;
+
+    while( true ){
+        std::cout << inputStr_ << std::endl;
+        std::getline(std::cin,number);
+        std::istringstream iss( number );
+        iss >> value >> std::ws;
+        if ( !(iss.fail() or !iss.eof()) ) {
+            return value;
+        }
+    }
+
+}
+
+
+int main(){
+
+    auto val = getInteger("Forneca um inteiro : ");
+
+}
